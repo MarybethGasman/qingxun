33832984ae1d21f714ec02e9d2239ce814a1ca3a
MarybethGasman <2934482133@qq.com>
2023-01-11 08:46:00 +0800

    bilibili

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="css">
+body {
+  background: #f6f6f6;
+  color: #121212;
+  font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, PingFang SC,
+    Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei,
+    sans-serif;
+  font-size: 15px;
+}
+</style>
